fad93b231049cfa3b04497941013fc877972f367
Julio Henrique Bitencourt <julio.henrique.b@gmail.com>
2019-02-27 23:41:54 -0300

    - Introducing FlavorBanner

--- lib/src/app/ui/pages/home_page.dart
+++ lib/src/app/ui/pages/home_page.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:ready_to_go/src/config/flavor_banner.dart';
 import 'package:ready_to_go/src/config/flavor_config.dart';
 
 class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Flutter Ready to Go')),
-      body: Center(child: Text("Flavor: ${FlavorConfig.instance.name}")),
+    return FlavorBanner(
+      child: Scaffold(
+        appBar: AppBar(title: Text('Flutter Ready to Go')),
+        body: Center(child: Text("Flavor: ${FlavorConfig.instance.name}")),
+      ),
     );
   }
 
---
+++ lib/src/config/flavor_banner.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:ready_to_go/src/config/flavor_config.dart';
+
+class FlavorBanner extends StatelessWidget {
+
+  final Widget child;
+  BannerConfig bannerConfig;
+
+  FlavorBanner({@required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    if(FlavorConfig.isProduction()) return child;
+
+    bannerConfig ??= _getDefaultBanner();
+
+    return Stack(
+      children: <Widget>[
+        child,
+        _buildBanner(context)
+      ],
+    );
+  }
+
+  BannerConfig _getDefaultBanner() {
+    return BannerConfig(
+      bannerName: FlavorConfig.instance.name,
+      bannerColor: FlavorConfig.instance.color
+    );
+  }
+
+  Widget _buildBanner(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      child: Container(
+        width: 50,
+        height: 50,
+        child: CustomPaint(
+          painter: BannerPainter(
+              message: bannerConfig.bannerName,
+              textDirection: Directionality.of(context),
+              layoutDirection: Directionality.of(context),
+              location: BannerLocation.topStart,
+              color: bannerConfig.bannerColor
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BannerConfig {
+
+  final String bannerName;
+  final Color bannerColor;
+
+  BannerConfig({
+    @required String this.bannerName,
+    @required Color this.bannerColor});
+
+}
