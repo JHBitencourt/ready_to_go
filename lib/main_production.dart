c08d89ded4d56585ec2fb93ffe094cb422cd632c
Julio Henrique Bitencourt <julio.henrique.b@gmail.com>
2019-03-02 12:51:46 -0300

    - Url based on the raw content from GitHub

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -4,7 +4,7 @@ import 'package:ready_to_go/src/config/flavor_config.dart';
 
 void main() {
   FlavorConfig(flavor: Flavor.PRODUCTION,
-      values: FlavorValues(baseUrl: "prod"));
+      values: FlavorValues(baseUrl: "https://raw.githubusercontent.com/JHBitencourt/ready_to_go/master/lib/json/person_production.json"));
 
   runApp(MyApp());
 }
--- lib/main_qa.dart
+++ lib/main_qa.dart
@@ -5,7 +5,7 @@ import 'package:ready_to_go/src/config/flavor_config.dart';
 void main() {
   FlavorConfig(flavor: Flavor.QA,
       color: Colors.deepPurpleAccent,
-      values: FlavorValues(baseUrl: "qa"));
+      values: FlavorValues(baseUrl: "https://raw.githubusercontent.com/JHBitencourt/ready_to_go/master/lib/json/person_qa.json"));
 
   runApp(MyApp());
 }
